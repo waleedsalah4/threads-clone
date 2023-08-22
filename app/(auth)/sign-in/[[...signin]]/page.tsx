4a78a7659a2ccc166eb83393fb2ee076ae2bf032
waleed salah <walidsalah652@gmail.com>
2023-08-22 18:51:32 +0300

    add fetch threads and thread by id actions, built a thread a card, and start building comment form

--- app/(auth)/sign-in/[[...signin]]/page.tsx
+++ app/(auth)/sign-in/[[...signin]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <section className="flex justify-center items-center h-screen">
+      <SignIn />
+    </section>
+  );
 }
