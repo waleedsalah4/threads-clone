660c70ed7a21af8ce7b07b5c130b76cdd7c8dd18
waleed salah <walidsalah652@gmail.com>
2023-09-14 20:32:07 +0300

    fix

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -69,6 +69,8 @@ export async function createThread({
       { _id: 1 }
     );
 
+    // console.log("commuintyId ====>", communityId);
+
     const createdThread = await Thread.create({
       text,
       author,
